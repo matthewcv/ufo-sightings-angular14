f9ebf5946cfab74435e49b4d13b8a15b2d83c38d
matthewcv <accounts@matthewcv.com>
2017-01-22 16:53:46 -0600

    starting the UI router

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
 (function(){
     angular.module("ufo-sightings", [])  //tells angular that we are creating a module named 'ufo-sightings' with no dependencies.
-        
+                .config(ConfigureApp)
+
+
+
+    function ConfigureApp(locationProvider){
+        locationProvider.html5Mode(true);
+
+    }
+
+    ConfigureApp.$inject = ["$locationProvider"]
 })();
