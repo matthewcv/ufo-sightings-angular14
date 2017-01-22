a60d3027387d83bf5d590a2a0df27baaf39b6c7b
matthewcv <accounts@matthewcv.com>
2017-01-22 17:23:26 -0600

    removed functionality from app-controller.js

--- app-controller.js
+++ app-controller.js
@@ -5,48 +5,10 @@
         .controller('AppController', AppController);    //register the controller with the module.
 
     //define the controller. 
-    function AppController(windowService, userService, $scope, ufoSightingsService) {
-        
+    function AppController( userService) {
         this.userService = userService;
-        this.ufoSightingsService = ufoSightingsService;
-        this.windowService = windowService;
-
-        this.scope = $scope;
-        var that = this;
-        this.scope.$on("UfoSightingReported", function(evt, report){
-            that.addUfoReport(report);
-        });
-
-        this.ufoSightingsService.getUfoSightings().then(function(data){
-            that.ufoReports = data;
-        });
     }
     
-    AppController.$inject = ['$window','userService','$scope','ufoSightingsService'];
-
-    
-    AppController.prototype.addUfoReport = function(sighting){
-        var that = this;
-        this.ufoSightingsService.saveUfoSightingReport(sighting).then(function(savedSighting){
-            that.ufoReports.splice(0,0,savedSighting);
-            that.windowService.alert('Thankyou, ' + that.userService.userName + '.\nYour sighting has been reported.');
-        })
-        
-    }
-    
-   
-   /**
-    * this is what a ufo report object looks like 
- {
-    "id": number;
-    "description": string;
-    "occurred": string;
-    "reported": string;
-    "posted": string;
-    "shape": string;
-    "duration": string;
-    "city": string;
-    "state": string;
-} */ 
+    AppController.$inject = ['userService'];
    
 })();
