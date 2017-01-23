cd1c21884d7fd994ddaeb066cc60f8ffa90fc545
matthewcv <accounts@matthewcv.com>
2017-01-23 12:59:29 -0600

    created report-sighting-controller test

---
+++ report-sighting-controller.test.js
@@ -0,0 +1,11 @@
+describe("the report sighting controller", function(){
+
+    beforeEach(module('ufo-sightings'));
+
+    it("should save the sighting when the UfoSightingReported event is emitted", inject(function(){
+
+
+        
+    }))
+
+})
