766b4d5f10cad420797232ef9aef9950d421d8ea
matthewcv <accounts@matthewcv.com>
2017-01-22 18:58:50 -0600

    set up home and report-sighting states

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
 (function(){
-    angular.module("ufo-sightings", [])  //tells angular that we are creating a module named 'ufo-sightings' with no dependencies.
+    angular.module("ufo-sightings", ["ui.router"])  //tells angular that we are creating a module named 'ufo-sightings' with no dependencies.
                 .config(ConfigureApp)
 
 
 
-    function ConfigureApp(locationProvider){
+    function ConfigureApp(locationProvider,stateProvider){
         locationProvider.html5Mode(true);
+        stateProvider.state(
+            "home", 
+            {
+                url:"/",
+                templateUrl:"home-view.html",
+                controller:"HomeController as ctrl",
+            }
+        )
+        .state(
+            "report-sighting",
+            {
+                url:"/report-sighting",
+                templateUrl:"report-sighting-view.html",
+                controller:"ReportSightingController as ctrl",
+            }
+        );
 
     }
 
-    ConfigureApp.$inject = ["$locationProvider"]
+    ConfigureApp.$inject = ["$locationProvider", "$stateProvider"]
 })();
